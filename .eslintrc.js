198e82ca60a5306d0d3f33813423a6ca92bff049
the0ffh <26150100+the0ffh@users.noreply.github.com>
2023-05-19 21:24:06 +0200

    update: eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,10 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'no-console': 'error',
-    'no-warning-comments': 'error',
+    '@typescript-eslint/no-console': 'error',
+    '@typescript-eslint/no-warning-comments': 'error',
+    '@typescript-eslint/prefer-arrow-callback': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-expressions': 'error',
   },
 };
