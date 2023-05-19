31420c4c61cd619f4d0141c858d2cc171fe9658e
the0ffh <26150100+the0ffh@users.noreply.github.com>
2023-05-19 10:35:27 +0200

    set up user dto use hardcoded data source params

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,13 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as path from 'path';
-import * as process from 'process';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: process.env.MYSQL_DATABASE_HOST,
-  port: parseInt(process.env.MYSQL_DATABASE_PORT),
-  database: process.env.MYSQL_DATABASE,
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: '127.0.0.1',
+  port: 3306,
+  database: 'devdb',
+  username: 'dev',
+  password: 'devpw',
   entities: [path.join(__dirname, 'src/**/*.entity.ts')],
   synchronize: false,
   migrations: [path.join(__dirname, 'migration/**/*.ts')],
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsString()
+  address: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  birthdate: string;
+}
