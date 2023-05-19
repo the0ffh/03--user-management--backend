c20081988946d3f636dac76f072b9f4ecd9ac3b2
the0ffh <26150100+the0ffh@users.noreply.github.com>
2023-05-19 02:06:27 +0200

    set up user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  readonly id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  address: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  birthdate: string;
+}
